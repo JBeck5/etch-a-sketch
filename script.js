de433eecffced5ab4c5f822b787ef3c3cfd6b2e3
JBeck5 <73002731+JBeck5@users.noreply.github.com>
2024-10-19 11:49:00 -0700

    checking

---
+++ script.js
@@ -0,0 +1,12 @@
+let container = document.getElementById('sketch-container');
+
+document.querySelector("#clear-button").addEventListener("click", resetSketch);
+
+let trace = documentquerySelector("#sketch-container");
+trace.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "black";
+});
+
+function resetSketch() {
+    
+}
