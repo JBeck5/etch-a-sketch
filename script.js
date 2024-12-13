0c35c6beb581eff91c25a532fbc371c51e752d91
JBeck5 <73002731+JBeck5@users.noreply.github.com>
2024-12-13 11:00:57 -0800

    set baseline for grid

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-let container = document.getElementById('sketch-container');
+let container = document.querySelector(".mainContainer");
+const grid = document.getElementById('sketch-container');
 
-document.querySelector("#clear-button").addEventListener("click", resetSketch);
 
-let trace = documentquerySelector("#sketch-container");
+
+const GRID_SIZE = 16;
+createNewGrid(GRID_SIZE);
+
+function createNewGrid(size) {
+    let sizeOfGrid = Math.pow(size, 2);
+    console.log(sizeOfGrid);
+    for (let i = 0; i < sizeOfGrid; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("pixel");
+        grid.appendChild(newDiv);
+    }
+
+    //formating each pixel in grid
+    let newHeight = newWidth = 256 / size;
+    console.log(newHeight);
+    console.log(newWidth);
+    const allPixels = document.querySelectorAll(".pixel");
+    allPixels.forEach(element => {
+        element.style.height = newHeight.toString() + "px";
+        element.style.width = newWidth.toString() + "px";
+    })
+}
+
+/*document.querySelector("#clear-button").addEventListener("click", resetSketch);
 trace.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "black";
 });
@@ -10,3 +34,4 @@ trace.addEventListener("mouseover", (e) => {
 function resetSketch() {
     
 }
+*/
